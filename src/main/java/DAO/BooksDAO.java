918561871da8eaeda8f970546580cfa6f8d048de
An Ho <an.nguyen.thein.ho@gmail.com>
2025-04-30 13:18:56 -0700

    Initial Work

--- src/main/java/DAO/BooksDAO.java
+++ src/main/java/DAO/BooksDAO.java
@@ -1,5 +1,37 @@
 package DAO;
 
-public class BooksDAO {
+import java.sql.SQLException;
+import java.util.List;
 
+public class BooksDAO implements DAO {
+
+    @Override
+    public Object get(int id) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public List getAll() throws SQLException {
+        return List.of();
+    }
+
+    @Override
+    public int save(Object o) throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public int insert(Object o) throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public int update(Object o) throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public int markInactive(int id) throws SQLException {
+        return 0;
+    }
 }
