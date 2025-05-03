b7c6b2279dc158051d8f8353f1747ddf135dea96
An Ho <an.nguyen.thein.ho@gmail.com>
2025-05-03 12:08:35 -0700

    Book dao get

--- src/main/java/DAO/BookDAO.java
+++ src/main/java/DAO/BookDAO.java
@@ -1,9 +1,11 @@
 package DAO;
 
+import POJO.Book;
 import com.zaxxer.hikari.HikariDataSource;
 
 import java.sql.*;
 import java.util.List;
+import java.sql.SQLException;
 
 public class BookDAO implements DAO {
     HikariDataSource ds = DBConnectionPool.getDataSource();
@@ -11,18 +13,41 @@ public class BookDAO implements DAO {
 
     @Override
     public Object get(int id)  {
-        Connection connection = ds.getConnection();
+        Connection connection = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
 
-        String query = "SELECT * FROM books WHERE id = ?";
-        PreparedStatement prpStmt = connection.prepareStatement(query);
-        prpStmt.setInt(1, id);
-        ResultSet rs = prpStmt.executeQuery();
+        try{
+            connection = ds.getConnection();
+            String query = "SELECT * FROM books WHERE id = ?";
+            ps = connection.prepareStatement(query);
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+
+            if(rs.next()){
+                Book book = new Book(rs.getInt("book_id"), rs.getInt("book_info_id"), rs.getInt("library_id"));
+                return book;
+            }
+        }
+        catch(SQLException e){
+            e.printStackTrace(); //can have more robust logging
+        }
+        finally {
+            try { rs.close(); } catch (Exception e) { /* Ignored */ }
+            try { ps.close(); } catch (Exception e) { /* Ignored */ }
+            try { connection.close(); } catch (Exception e) { /* Ignored */ }
+        }
         return null;
     }
 
     @Override
     public List getAll() {
         return List.of();
+    }
+
+    @Override
+    public int insert(Object o) {
+        return 0;
     }
 
     @Override
